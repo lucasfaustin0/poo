3c5cf44ea24b4d8cdd2ad3edf885e7ad7ad94af0
lucasfaustin0 <limalucasfaustino@gmail.com>
2025-09-01 00:06:00 -0300

    revisao.java

---
+++ revisao.java
@@ -0,0 +1,9 @@
+1° questao- /*
+- Classe é como um molde, onde definimos atributos (dados) e métodos (ações).
+- Objeto é a representação real criada a partir da classe.
+- Instância é o processo de criar um objeto a partir de uma classe.
+*/
+2° questao- /*
+- Private protege os atributos/métodos, permitindo acesso só dentro da própria classe.
+- Public permite que qualquer parte do programa acesse.
+*/
